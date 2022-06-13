bd28248a7b32baa87fb3017455685bed0735887d
mohamed hassan <mohamedisaaq0@gmail.com>
2022-06-13 18:28:53 +0100

    linking pages by inquirer

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-// TODO: Include packages needed for this application
+// require modules
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// linking to page where the README is developed
+const generatePage = require("./utils/generateMarkdown.js");
